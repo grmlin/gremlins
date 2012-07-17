68521b7b2f6cceeede2e82e29bc17a19dc5fd3c8
hrdwr <mail@andreas-wehr.de>
2012-07-17 16:22:52 +0200

    relative path

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-require.config({
-    //baseUrl : "./",
-    paths : {
-        "jquery" : "jquery-1.7.1.min",
-        "gremlinjs" : "gremlin.min"
-    }
-});
-
 require(["gremlinjs"], function (gremlinjs) {
     'use strict';
-    gremlinjs.getLoader("gremlins/").load();
+    gremlinjs.getLoader("js/gremlins/").load();
 });
