3f2cb4ff19159a9da2109b59a726e322096ff57d
Andreas <me@grml.in>
2013-08-29 22:17:24 +0200

    tests cleaned

--- test/js/gremlins/Buddy.js
+++
@@ -1,3 +0,0 @@
-Gremlin.define("Buddy", function () {
-  $(this.el).find('.content').html("<h1>Hello World, Buddy here!</h1>").fadeTo(0, 0).fadeTo(500, 1);
-});
--- test/js/gremlins/cs/Espresso.coffee
+++
@@ -1,6 +0,0 @@
-class Espresso extends G.Gizmo
-  constructor : ->
-    super
-    @el.innerHTML = "Espresso #{@id}, hmmmm"
-
-G.add "Espresso", Espresso
