82d055b748a11695450f7d009c084b325d25fa6a
Andreas Wehr <aw@decaf.de>
2017-01-20 11:02:45 +0100

    eliminating persistant references to the dom element

--- src/Data.js
+++ src/Data.js
@@ -1,52 +1,22 @@
-const uuid = require('./uuid');
-
-const exp = `gremlins_${uuid()}`;
-const cache = {};
 let pendingSearches = [];
 
-const gremlinId = (function gremlinId() {
-  let id = 1;
-  return () => id++;
-}());
-
-const hasId = (element) => element[exp] !== undefined;
-const setId = (element) => element[exp] = gremlinId(); // eslint-disable-line no-param-reassign
-const getId = (element) => hasId(element) ? element[exp] : setId(element);
+const hasId = (element) => element._gid !== undefined;
 
 module.exports = {
-  addGremlin(gremlin, element) {
-    const id = getId(element);
-
-    if (cache[id] !== undefined) {
-      console.warn(`You can't add another gremlin to this element, it already uses one!`, element); // eslint-disable-line no-console, max-len
-    } else {
-      cache[id] = gremlin;
-    }
-
+  addGremlin(id) {
     pendingSearches = pendingSearches.filter((search) => {
-      const wasSearchedFor = search.element === element;
+      const wasSearchedFor = search.element._gid === id;
       if (wasSearchedFor) {
-        search.created(gremlin);
+        search.resolve();
       }
 
       return !wasSearchedFor;
     });
   },
-  getGremlin(element) {
-    const id = getId(element);
-    const gremlin = cache[id];
-
-    if (gremlin === undefined) {
-      // console.warn(`This dom element does not use any gremlins!`, element);
-    }
-    return gremlin === undefined ? null : gremlin;
-  },
   getGremlinAsync(element, timeout = null) {
     return new Promise((resolve) => {
-      const currentGremlin = this.getGremlin(element);
-
-      if (currentGremlin !== null) {
-        resolve(currentGremlin);
+      if (hasId(element)) {
+        setTimeout(() => resolve(element), 10);
       } else {
         const gremlinNotFoundTimeout = timeout !== null ? setTimeout(() => {
           resolve(null);
@@ -54,9 +24,9 @@ module.exports = {
 
         pendingSearches.push({
           element,
-          created(createdGremlin) {
+          resolve() {
             clearTimeout(gremlinNotFoundTimeout);
-            resolve(createdGremlin);
+            resolve(element);
           },
         });
       }
--- src/GremlinElement.js
+++ src/GremlinElement.js
@@ -1,5 +1,5 @@
-const Factory = require('./Factory');
 const Data = require('./Data');
+const uuid = require('./uuid');
 
 const canRegisterElements = typeof document.registerElement === 'function';
 
@@ -9,85 +9,50 @@ if (!canRegisterElements) {
   );
 }
 
+const gremlinId = () => `gremlins_${uuid()}`;
 const styleElement = document.createElement('style');
 let styleSheet;
 
 document.head.appendChild(styleElement);
 styleSheet = styleElement.sheet;
 
-function createInstance(element, Spec) {
-  const existingGremlins = Data.getGremlin(element);
-
-  if (existingGremlins === null) {
-    const gremlin = Factory.createInstance(element, Spec);
-    Data.addGremlin(gremlin, element);
-
-    if (typeof gremlin.initialize === 'function') {
-      console.warn(
-        `<${element.tagName} />\n` +
-        'the use of the `initialize` callback of a gremlin component is deprecated. ' +
-        'Use the `created` callback instead.');
-      gremlin.initialize();
-    } else {
-      gremlin.created();
-    }
-  } else {
-    // console.warn('exisiting gremlin found');
-  }
-}
-
-function attachInstance(element) {
-  const gremlin = Data.getGremlin(element);
-  gremlin.attached();
-}
-
-function detachInstance(element) {
-  const gremlin = Data.getGremlin(element);
-
-  if (typeof gremlin.destroy === 'function') {
-    console.warn(
-      `<${element.tagName} />\n` +
-      'the use of the `destroy` callback of a gremlin component is deprecated. Use ' +
-      'the `detached` callback instead.');
-    gremlin.destroy();
-  } else {
-    gremlin.detached();
-  }
-}
-
-function updateAttr(element, name, previousValue, value) {
-  const gremlin = Data.getGremlin(element);
-
-  if (gremlin !== null) {
-    gremlin.attributeDidChange(name, previousValue, value);
-  }
-}
-
-
 module.exports = {
   register(tagName, Spec) {
+    // TODO test for reserved function names ['createdCallback', 'attachedCallback', '']
+
     const proto = {
       createdCallback: {
         value() {
-          createInstance(this, Spec);
+          this._gid = gremlinId();
+
+          Data.addGremlin(this._gid);
+          this.created();
         },
+        writable: false,
       },
       attachedCallback: {
         value() {
-          attachInstance(this);
+          this.attached();
         },
       },
       detachedCallback: {
         value() {
-          detachInstance(this);
+          this.detached();
         },
       },
       attributeChangedCallback: {
         value(name, previousValue, value) {
-          updateAttr(this, name, previousValue, value);
+          this.attributeDidChange(name, previousValue, value);
         },
       },
     };
+
+    for (const key in Spec) { // eslint-disable-line guard-for-in
+      proto[key] = {
+        value: Spec[key],
+      };
+    }
+
 
     // insert the rule BEFORE registering the element. This is important because they may be inline
     // otherwise when first initialized.
